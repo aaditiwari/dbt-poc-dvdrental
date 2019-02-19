f529f73b655c3343319200e16f16e3bcdc9f82ae
aaditiwari <aadi.tiwari@gmail.com>
2019-02-19 17:13:26 +0530

    Add files via upload

---
+++ models/tables/address.sql
@@ -0,0 +1,11 @@
+
+-- Welcome to your dbt model!
+-- Configurations for models done directly within
+-- the SQL file will override configurations stated in dbt_project.yml
+
+-- Change 'view' to 'table' or vice-versa as per requirement
+
+{{ config(materialized='table') }}
+
+
+select * from public.address
